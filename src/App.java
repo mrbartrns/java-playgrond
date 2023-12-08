8af3737f8c56435fe283b25b8ffbb8f03e8dd5ba
Junhyuck <mrbartrns@naver.com>
2023-12-08 12:00:55 +0900

    package를 만들고 클래스 실행

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import animal.Animal;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Car myCar = new Car("Model 1");
+        Animal animal = new Animal();
+        animal.setName("Dog");
+        System.err.println(animal.getName());
+        System.out.println(myCar.getCarName());
     }
 }
+
+
+class Car {
+    private String carName;
+
+    Car(String name) {
+        carName = name;
+    }
+
+    String getCarName() {
+        return this.carName;
+    }
+}
+
---
+++ src/MyArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> apple = new ArrayList<>(Arrays.asList("a", "b", "c"));
+
+    }
+}
